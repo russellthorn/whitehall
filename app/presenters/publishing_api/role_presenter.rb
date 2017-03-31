abb4a95df7e17caf1f7fd6f8e9000ec2c0e13ab8
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2017-03-31 14:57:54 +0100

    Add role presenter

---
+++ app/presenters/publishing_api/role_presenter.rb
@@ -0,0 +1,36 @@
+module PublishingApi
+  class RolePresenter
+    attr_accessor :item
+    attr_accessor :update_type
+
+    def initialize(item, update_type: nil)
+      self.item = item
+      self.update_type = update_type || "major"
+    end
+
+    def content_id
+      item.content_id
+    end
+
+    def content
+      content = BaseItemPresenter.new(
+        item,
+        title: item.name,
+        need_ids: [],
+      ).base_attributes
+
+      content.merge!(
+        description: nil,
+        details: {},
+        document_type: item.class.name.underscore,
+        public_updated_at: item.updated_at,
+        rendering_app: Whitehall::RenderingApp::WHITEHALL_FRONTEND,
+        schema_name: "placeholder",
+      )
+    end
+
+    def links
+      LinksPresenter.new(item).extract([:organisations])
+    end
+  end
+end
